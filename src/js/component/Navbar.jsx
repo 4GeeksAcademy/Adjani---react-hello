9e883f14b1450927490ba0dc434903983f58d230
Adjani <133295232+Adjani09@users.noreply.github.com>
2023-09-02 03:07:53 +0000

    commit

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -6,11 +6,11 @@ const Navbar = () => {
             <div className="container-fluid" id="navbar">
                 <a className="navbar-brand text-white p-10" href="#">Landing Page</a>
             </div>
-            <div class="collapse navbar-collapse navbar-nav mr-5 ms-auto mb-2 mb-lg-0" id="navbarText">
-                <a class="nav-link text-white" href="#">Home </a>
-                <a class="nav-link text-white" href="#">About</a>
-                <a class="nav-link text-white" href="#">Services</a>
-                <a class="nav-link text-white" href="#">Contact</a>
+            <div className="collapse navbar-collapse navbar-nav mr-5 ms-auto mb-2 mb-lg-0" id="navbarText">
+                <a className="nav-link text-white" href="#">Home </a>
+                <a className="nav-link text-white" href="#">About</a>
+                <a className="nav-link text-white" href="#">Services</a>
+                <a className="nav-link text-white" href="#">Contact</a>
             </div>
         </nav>
     )
